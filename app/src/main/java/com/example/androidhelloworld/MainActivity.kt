ba2f761d7a11175b9546496da1b7bb1baea80a80
gaku yamamoto <githubgemini1124@gmail.com>
2021-02-11 14:38:38 +0900

    implement button to change text

--- app/src/main/java/com/example/androidhelloworld/MainActivity.kt
+++ app/src/main/java/com/example/androidhelloworld/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.androidhelloworld
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
+@SuppressLint("WrongViewCast")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMeButton = findViewById<Button>(R.id.button)
+        var myTextView = findViewById<TextView>(R.id.textView)
+        clickMeButton.setOnClickListener {
+            myTextView.text = "new Text"
+        }
     }
 }
